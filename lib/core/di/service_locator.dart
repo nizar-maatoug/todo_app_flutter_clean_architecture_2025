5010bc820bee78abbbc99d2e9bdd3755db7806ae
Nizar MAATOUG <nizar.maatoug@hotmail.com>
2025-11-09 06:51:59 +0100

    setup firebase

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,10 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:get_it/get_it.dart';
+import 'package:todo_app/core/firebase/auth_service.dart';
 import 'package:todo_app/core/router/app_router.dart';
 import 'package:todo_app/features/tasks/presentation/blocs/bloc/switchtheme_bloc.dart';
+import 'package:todo_app/firebase_options.dart';
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
+  // Initialize Firebase first
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Register Auth service
+  sl.registerLazySingleton<AuthService>(
+    () => AuthService(FirebaseAuth.instance),
+  );
+
   // find and instanciate AppRouter
   sl.registerLazySingleton(() => AppRouter());
 
---
+++ lib/core/firebase/auth_service.dart
@@ -0,0 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth;
+
+  AuthService(this._auth);
+
+  Stream<User?> get authStateChanges => _auth.authStateChanges();
+
+  User? get currentUser => _auth.currentUser;
+
+  Future<UserCredential> signInWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    return await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<UserCredential> signUpWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    return await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+
+  Future<void> sendPasswordResetEmail(String email) async {
+    await _auth.sendPasswordResetEmail(email: email);
+  }
+}
